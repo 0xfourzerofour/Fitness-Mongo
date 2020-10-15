67714648e787141f7d37d8842ee210a73d0862c3
Josh Pauline <joshpauline@gmail.com>
2020-10-15 12:09:57 +1100

    added routes from previous project to work with

---
+++ backend/routes/posts.js
@@ -0,0 +1,157 @@
+const router = require('express').Router();
+let post = require('../models/post.model');
+let User = require('../models/user.model');
+
+//This is where we import our verify middleware that allows us
+//to check and see if the users passes a valid auth-token to the
+//request.
+
+//all request without a valid token will be denied.
+
+const verify = require('./verify');
+
+//this is the route which is used when there is a new post added to the DB
+//we first extract all the content from the request body and then we check to see if the user
+//has used any mentions in the text. if their are mentions they will be added to the mentions
+//array for that post. We also check to see how long the text is as there is a max length of 250 characters for each post
+
+router.route('/new').post(verify, (req, res) => {
+  const content = req.body.content;
+  const tags = req.body.tags;
+  const id = req.body.user.id;
+  const username = req.body.user.username;
+
+  let mentions = [];
+
+  if (content.match(/\B\@([\w\-]+)/g)) {
+    content.match(/\B\@([\w\-]+)/g).forEach((mention) => {
+      mentions = [...mentions, mention.substring(1, mention.length)];
+    });
+  }
+
+  if (content.length > 250) {
+    res.status(400).json('string too long');
+  }
+
+  const NewPost = new post({
+    content,
+    tags,
+    mentions,
+    id,
+    username,
+  });
+
+  NewPost.save()
+    .then(() => {
+      res.json('post created');
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+//this route is used for the profile page to get all the post that
+//the current user has psoted and return them.
+
+router.route('/').get(verify, (req, res) => {
+  post
+    .find({
+      username: { $eq: req.headers.user },
+    })
+    .sort({ createdAt: -1 })
+    .then((posts) => res.json(posts))
+    .catch((err) => res.status(400).json('Error: ' + err));
+});
+
+//need to make async request to get followed users then
+//get all posts from those users and then sort them by when
+//the post was created
+
+router.route('/feed').get(verify, async (req, res) => {
+  const users = User.findById(req.user).then((response) => {
+    let arrayUsers = response.following;
+
+    arrayUsers.push(response.username);
+
+    return arrayUsers;
+  });
+
+  post
+    .find({
+      username: { $in: await users },
+    })
+    .sort({ createdAt: -1 })
+    .then((response) => {
+      res.json(response);
+    });
+});
+
+//public route for the homepage that only returns the most recent 20 posts
+
+router.route('/recents_noauth').get((req, res) => {
+  post
+    .find()
+    .sort({ createdAt: -1 })
+    .limit(20)
+    .then((resp) => {
+      res.json(resp);
+    });
+});
+
+//using the push function to append comment information to existing
+//database array
+
+router.route('/comment').put(verify, (req, res) => {
+  post
+    .updateOne(
+      { _id: req.body.postId },
+      {
+        $push: {
+          comments: [req.body.commentInfo],
+        },
+      }
+    )
+    .then((response) => {
+      res.json(response);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+//using addToSet operator becasue you can only like a post once
+//this checks if the id is already in the likes array
+
+router.route('/like').put(verify, (req, res) => {
+  post
+    .updateOne(
+      { _id: req.body.postId },
+      {
+        $addToSet: {
+          likes: [req.user],
+        },
+      }
+    )
+    .then((response) => {
+      res.json(response);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+//return all posts that have the specified tag within their tag arrays
+//I used the elemMatch query to check each individual tag in the array and return that
+//post if one of the tags is equal to they header tag
+
+router.route('/tag').get(verify, async (req, res) => {
+  post
+    .find({
+      tags: { $elemMatch: { $eq: req.headers.tag } },
+    })
+    .then((response) => {
+      res.json(response);
+    });
+});
+
+module.exports = router;
---
+++ backend/routes/users.js
@@ -0,0 +1,148 @@
+const router = require('express').Router();
+const bcrypt = require('bcryptjs');
+const multer = require('multer');
+const verify = require('./verify');
+let User = require('../models/user.model');
+
+//I used multer to specify the location to store images on the server
+//and to automatically name the files based on their original name and
+//the current timestamp so that we do not have any duplicates.
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './images/');
+  },
+  filename: function (req, file, cb) {
+    cb(null, new Date().toISOString() + file.originalname);
+  },
+});
+
+const upload = multer({ storage: storage });
+
+//This is a simply API request that takes the request username and returns all the detail
+//for the requested user. (Exluding Hashed passwords)
+router.route('/userdetails').post(verify, (req, res) => {
+  User.findOne({ username: req.body.id })
+    .then((users) =>
+      res.json({
+        username: users.username,
+        imageUrl: users.imageUrl,
+        following: users.following,
+        bio: users.bio,
+      })
+    )
+    .catch((err) => res.status(400).json('Error: ' + err));
+});
+
+//returns id of user based on the passed '@username' format
+router.route('/id').get(verify, (req, res) => {
+  const id = req.headers.user.split('@');
+
+  console.log(id[1]);
+
+  User.findOne({
+    username: {
+      $eq: id[1],
+    },
+  })
+    .then((resp) => {
+      res.json(resp._id);
+    })
+    .catch((err) => {
+      res.json('no user found');
+    });
+});
+
+//This route returns the current user based on the ID that is returned
+//from the verify middleware function (this is called in our App.js file on load)
+router.route('/currentuser').get(verify, (req, res) => {
+  User.findById(req.user)
+    .then((user) =>
+      res.json({
+        id: user._id,
+        username: user.username,
+        following: user.following,
+        bio: user.bio,
+      })
+    )
+    .catch((err) => res.status(400).json('Error: ' + err));
+});
+
+//this is a regular expression function created to help with
+//the search route so that we can have a fuzzy search implementation.
+function escapeRegex(text) {
+  return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&');
+}
+
+//we implement the above function so that we can pass the regular expression
+//into the find function based on the username matchs.
+
+//I did this because I found it difficult to get the correct results using
+//the inbuilt mongo $regex and $search queries
+router.route('/search').post(verify, (req, res) => {
+  const regex = new RegExp(escapeRegex(req.body.searchString), 'gi');
+
+  User.find({
+    username: regex,
+  })
+
+    .then((users) => res.json(users))
+    .catch((err) => res.status(400).json('Error: ' + err));
+});
+
+//This is the user creation route that is called when a user registers
+//with the application.
+
+//First we extract the username, password, bio and image from the form data
+//and then convert the username to lowercase so that all users are lowercase
+//I then used the bcrypt library to create a Hashed password to be stored in
+//the database instead of leaving them as plain text.
+
+//Once the password is converted we pass the new hashed password into the database
+//along with the other fields.
+
+//we are able to get the image path from the uploaded image as we are using
+//multer as a middleware to create the path and store the file before the request is processed
+router.route('/add').post(upload.single('image'), async (req, res) => {
+  const username = req.body.username;
+  const lowered = username.toLowerCase();
+  const imageUrl = req.file.path;
+  const bio = req.body.bio;
+
+  const salt = await bcrypt.genSalt(10);
+  const hashPassword = await bcrypt.hash(req.body.password, salt);
+
+  const newUser = new User({
+    username: lowered,
+    password: hashPassword,
+    bio,
+    following: [],
+    imageUrl,
+  });
+
+  try {
+    newUser
+      .save()
+      .then(() => res.json('user added'))
+      .catch((err) => res.status(400).json('Error: ' + err));
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+//This route just takes the current user from the auth-token
+//and adds them to the requested username to their following array
+router.route('/follow').put(verify, (req, res) => {
+  User.updateOne(
+    { _id: req.user },
+    {
+      $addToSet: {
+        following: req.body.user,
+      },
+    }
+  ).then((resp) => {
+    res.json(resp);
+  });
+});
+
+module.exports = router;
