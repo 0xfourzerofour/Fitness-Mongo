9e98e52b39601fca3ab780c434dd10974d726a57
juliancesaro <julianjcesaro@gmail.com>
2020-10-18 19:14:38 +1100

    Added bootstrap login modal

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,11 +4,19 @@ import Topnav from "./components/topnav/Topnav"
 import LoginForm from "./components/loginForm/LoginForm"
 
 function App() {
-  const [showLoginForm, setShowLoginForm] = useState(false)
+  const [showLoginForm, setShowLoginForm] = useState(true)
   return (
     <div className="App">
-      <Topnav />
-      {showLoginForm ? <LoginForm /> : null}
+      <Topnav
+        showLoginForm={showLoginForm}
+        setShowLoginForm={setShowLoginForm}
+      />
+      {showLoginForm ? (
+        <LoginForm
+          showLoginForm={showLoginForm}
+          setShowLoginForm={setShowLoginForm}
+        />
+      ) : null}
     </div>
   )
 }
--- frontend/src/components/topnav/Topnav.js
+++ frontend/src/components/topnav/Topnav.js
@@ -1,12 +1,9 @@
 import React from "react"
 import Navbar from "react-bootstrap/Navbar"
 import Nav from "react-bootstrap/Nav"
-import NavDropdown from "react-bootstrap/NavDropdown"
-import Form from "react-bootstrap/Form"
-import FormControl from "react-bootstrap/FormControl"
 import Button from "react-bootstrap/Button"
 
-const Topnav = () => {
+const Topnav = ({ showLoginForm, setShowLoginForm }) => {
   return (
     <Navbar bg="dark" variant="dark" expand="lg">
       <Navbar.Brand href="#home">Fit</Navbar.Brand>
@@ -16,7 +13,7 @@ const Topnav = () => {
           <Nav.Link href="#home">Home</Nav.Link>
           <Nav.Link href="#About">About</Nav.Link>
         </Nav>
-        <Button>Login</Button>
+        <Button onClick={() => setShowLoginForm(!showLoginForm)}>Login</Button>
       </Navbar.Collapse>
     </Navbar>
   )
