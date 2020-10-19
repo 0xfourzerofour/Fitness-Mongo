229561d33a3d32a96a4e3fa4370ce97b3fb1cb20
swarnim madan <swarnim.madan@students.mq.edu.au>
2020-10-19 13:16:19 +1100

    adding basic css layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,22 +4,22 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div className="row">
+     <div className="six columns">
+        <h1>
+        Hello User! 
+        <h3>
+        Please login to continue
+        </h3>
+        </h1>
+      </div>
+      <div className="six columns">
+        {/* Loginform  */}
+      </div>
     </div>
+          
+    // Further css can be added after loginform etc is done
   );
 }
 
