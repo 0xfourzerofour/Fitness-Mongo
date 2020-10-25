f9746f79d6856a8e0bea781364ff1aff7b57f864
juliancesaro <julianjcesaro@gmail.com>
2020-10-25 16:24:24 +1100

    Added register function and user, login services

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,21 @@
-import React, { useState } from "react"
-import "./App.css"
-import Topnav from "./components/topnav/Topnav"
-import LoginForm from "./components/loginForm/LoginForm"
-import RegisterForm from "./components/registerForm/RegisterForm"
+import React, { useState, useEffect } from 'react'
+import './App.css'
+import userService from './services/users'
+import Topnav from './components/topnav/Topnav'
+import LoginForm from './components/loginForm/LoginForm'
+import RegisterForm from './components/registerForm/RegisterForm'
+import Home from './components/home/Home'
 
 function App() {
+  const [user, setUser] = useState(null)
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedSpeakUser')
+    if (loggedUserJSON) {
+      const savedUser = JSON.parse(loggedUserJSON)
+      setUser(savedUser)
+      userService.setToken(savedUser.token)
+    }
+  }, [])
   const [showLoginForm, setShowLoginForm] = useState(false)
   const [showRegisterForm, setShowRegisterForm] = useState(true)
   return (
--- frontend/src/components/registerForm/RegisterForm.js
+++ frontend/src/components/registerForm/RegisterForm.js
@@ -1,16 +1,56 @@
-import React, { useState } from "react"
-import "./RegisterForm.css"
-import Modal from "react-bootstrap/Modal"
-import Form from "react-bootstrap/Form"
-import Button from "react-bootstrap/Button"
+import React, { useState } from 'react'
+import './RegisterForm.css'
+import userService from '../../services/users'
+import Modal from 'react-bootstrap/Modal'
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+import Alert from 'react-bootstrap/Alert'
 
 const RegisterForm = ({
   showRegisterForm,
   setShowRegisterForm,
   setShowLoginForm,
 }) => {
-  const [usernameVal, setUsernameVal] = useState("")
-  const [passwordVal, setPasswordVal] = useState("")
+  const [usernameVal, setUsernameVal] = useState('')
+  const [passwordVal, setPasswordVal] = useState('')
+  const [confirmPasswordVal, setConfirmPasswordVal] = useState('')
+  const [registerSuccess, setRegisterSuccess] = useState(null)
+  const [messageText, setMessageText] = useState('')
+  const [file, setFile] = useState(null)
+
+  const handleRegister = async (event) => {
+    event.preventDefault()
+    try {
+      if (passwordVal === confirmPasswordVal) {
+        await userService.create({
+          username: usernameVal,
+          password: passwordVal,
+          file: file,
+        })
+        setRegisterSuccess(true)
+        setMessageText('Account created!')
+        setTimeout(() => {
+          setShowRegisterForm(false)
+          setShowLoginForm(true)
+        }, 1000)
+      } else {
+        throw new Error("Passwords don't match!")
+      }
+    } catch (error) {
+      setRegisterSuccess(false)
+      if (error.message === "Passwords don't match!") {
+        setMessageText(error.message)
+      } else if (error.message === 'Bad password!') {
+        setMessageText(error.message)
+      } else if (
+        error.response.data.error.includes('is longer than the maximum')
+      ) {
+        setMessageText('Username too long!')
+      } else if (error.response.data.error.includes('to be unique')) {
+        setMessageText('Username taken!')
+      }
+    }
+  }
 
   const handleClose = () => {
     setShowRegisterForm(false)
@@ -21,12 +61,6 @@ const RegisterForm = ({
     setShowLoginForm(true)
   }
 
-  const handleRegister = async (event) => {
-    event.preventDefault()
-    // register API calls go here
-    handleClose()
-  }
-
   const handleUsernameChange = (event) => {
     setUsernameVal(event.target.value)
   }
@@ -35,12 +69,28 @@ const RegisterForm = ({
     setPasswordVal(event.target.value)
   }
 
+  const handleConfirmPasswordChange = (event) => {
+    setConfirmPasswordVal(event.target.value)
+  }
+
+  const handleImageChange = (event) => {
+    if (event.target.files && event.target.files[0]) {
+      let img = event.target.files[0]
+      setFile(URL.createObjectURL(img))
+    }
+  }
+
   return (
     <Modal show={showRegisterForm} onHide={handleClose}>
       <Modal.Header closeButton>
         <Modal.Title>Register</Modal.Title>
       </Modal.Header>
       <Modal.Body>
+        {registerSuccess === true ? (
+          <Alert variant="success">{messageText}</Alert>
+        ) : registerSuccess === false ? (
+          <Alert variant="danger">{messageText}</Alert>
+        ) : null}
         <Form onSubmit={handleRegister}>
           <Form.Group controlId="formBasicEmail">
             <Form.Label>Username</Form.Label>
@@ -63,8 +113,25 @@ const RegisterForm = ({
               Password must be x characters long.
             </Form.Text>
           </Form.Group>
+          <Form.Group controlId="formBasicConfirmPassword">
+            <Form.Label>Confirm Password</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Confirm Password"
+              value={confirmPasswordVal}
+              onChange={handleConfirmPasswordChange}
+            />
+          </Form.Group>
+          <Form.Group controlId="formFile">
+            <Form.Label>Upload avatar</Form.Label>
+            <Form.File
+              id="formControlFile"
+              onChange={handleImageChange}
+              name="image"
+            />
+          </Form.Group>
           <p>
-            Already have an account?{" "}
+            Already have an account?{' '}
             <span className="register-link" onClick={handleLoginClick}>
               login
             </span>
